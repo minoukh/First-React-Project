38d819f73a774f19233b435b32b09b97682ffef6
Minou <115370322+minoukh@users.noreply.github.com>
2023-08-21 09:39:10 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "black-t-50": "rgba(0,0,0,0.5)",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
